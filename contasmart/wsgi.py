409103e6da3199e8873bc34886cc57a6bbe1d9af
edu <valenzuela.edo@gmail.com>
2023-04-21 16:41:53 -0400

    refactor

--- contasmart/wsgi.py
+++ contasmart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangocrud project.
+WSGI config for contasmart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocrud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contasmart.settings')
 
 application = get_wsgi_application()
 
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocrud.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contasmart.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
